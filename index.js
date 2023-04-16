c5da4dca23a344de85242461472a9ad6e3ccf604
marwa aljmal <107772075+marwaaljmaal@users.noreply.github.com>
2023-04-16 13:20:40 +0200

    task2p2

--- index.js
+++ index.js
@@ -1,60 +1,10 @@
-class Rectangle {
-    constructor(width, height) {
-    this._width = null;
-    this._height = null;
-    this.width = width;
-    this.height = height;
-    }
-
-    get width() {
-    return this._width;
-    }
-
-    set width(value) {
-    if (!Number.isInteger(value)) {
-        throw new Error("width must be an integer value");
-    }
-    if (value <= 0) {
-        throw new Error("width must be a positive value");
-    }
-    this._width = value;
-    }
-
-    get height() {
-    return this._height;
-    }
-
-    set height(value) {
-    if (!Number.isInteger(value)) {
-        throw new Error("height must be an integer value");
-    }
-    if (value <= 0) {
-        throw new Error("height must be a positive value");
-    }
-    this._height = value;
-    }
-
-    area() {
-      return this.width * this.height;
-    }
-
-    perimeter() {
-      return 2 * (this.width + this.height);
-    }
-
-    draw(symbol) {
-    let row = "";
-    for (let i = 0; i < this.width; i++) {
-        row += symbol;
-    }
-    for (let i = 0; i < this.height; i++) {
-        console.log(row);
-    }
+class Square extends Rectangle {
+    constructor(side) {
+    super(side, side);
     }
 }
 
-
-let rec = new Rectangle(3, 5);
-    console.log(rec.area());  
-    console.log(rec.perimeter()); 
-rec.draw("0");
+var sq = new Square(4);
+    console.log(sq.area());  
+    console.log(sq.perimeter());  
+sq.draw("0");
